dde5a78174594a00251ffb91b633eb8d8054a790
Amin Riqky <aminriqky@gmail.com>
2022-09-19 17:51:19 +0700

    init

---
+++ Fungsi/ContohFungsi.java
@@ -0,0 +1,13 @@
+class ContohFungsi
+{
+	static void ucapSalam()
+	{
+		System.out.println(" ");
+		System.out.println("Selamat Pagi!");
+	}
+	
+	public static void main(String[] args)
+	{
+		ucapSalam();
+	}
+}
---
+++ Fungsi/ContohFungsi3.java
@@ -0,0 +1,15 @@
+class ContohFungsi3
+{
+	static int luasPersegi(int sisi)
+	{
+		
+		int luas = sisi*sisi;
+		return luas;
+	}
+	
+	public static void main(String[] args)
+	{
+		System.out.println(" ");
+		System.out.println("Luas Persegi = " + luasPersegi(5));
+	}
+}
